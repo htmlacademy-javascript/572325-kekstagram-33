e1d6f41cb32399dff0fa3b6160e02a3a9521f6b4
Keks <keks@htmlacademy.ru>
2024-11-22 11:55:30 +0300

    :heavy_check_mark: Сборка #9

--- 9/js/photo-upload.js
+++ 9/js/photo-upload.js
@@ -1,21 +1,24 @@
 import {miniPicturesContainer} from './thumbnails.js';
 import {isEscapeKey} from './util.js';
 
-const imgUploadForm = document.querySelector('#upload-select-image');
 const imgUploadInput = miniPicturesContainer.querySelector('#upload-file');
+const imgUploadForm = document.querySelector('#upload-select-image');
 const imgUploadOverlay = imgUploadForm.querySelector('.img-upload__overlay');
 const imgPreview = imgUploadOverlay.querySelector('.img-upload__preview img');
 const imgEffectsPreview = imgUploadOverlay.querySelectorAll('.effects__preview');
+const hashtagInput = imgUploadForm.querySelector('.text__hashtags');
+const descriptionField = imgUploadForm.querySelector('.text__description');
 
 const closeUploadModal = () => {
   imgUploadOverlay.classList.add('hidden');
   document.body.classList.remove('modal-open');
+  imgUploadForm.reset();
 };
 
 imgUploadOverlay.querySelector('#upload-cancel').addEventListener('click', closeUploadModal);
 
 const onDocumentKeydown = (event) => {
-  if (isEscapeKey(event)) {
+  if (isEscapeKey(event) && [hashtagInput, descriptionField].every((elem) => event.target !== elem)) {
     event.preventDefault();
     closeUploadModal();
   }
@@ -43,25 +46,29 @@ const pristine = new Pristine(imgUploadForm, {
 
 const regexp = /^#[a-zа-яё0-9]{1,19}$/i;
 
-const hashtagInput = imgUploadForm.querySelector('.text__hashtags');
 const uploadSubmitBtn = imgUploadForm.querySelector('#upload-submit');
 const MAX_HASHTAG_AMOUNT = 5;
+const MAX_SYMBOLS_AMOUNT = 140;
 let pristineErrorMsg;
 
-const isHashtagValid = (array) => array.every((hashtag) => {
+const isHashtagValid = (array) => {
   pristineErrorMsg = 'Невалидный хэштег';
-  return regexp.test(hashtag);
-});
+  return array.every((hashtag) => regexp.test(hashtag));
+};
 
-const isHashtagDuplicate = (array) => !array.every((hashtag, i) => {
+const isHashtagDuplicate = (array) => {
   pristineErrorMsg = 'Хэштеги не должны повторяться';
-  return array.includes(hashtag, i + 1);
-});
+  return array.filter((hashtag, i) => array.indexOf(hashtag) !== i).length;
+};
+
+const isAmountLessThan5 = (array) => {
+  pristineErrorMsg = 'Количество хэштегов превышает допустимое';
+  return array.length <= MAX_HASHTAG_AMOUNT;
+};
 
 const validateHashtags = (value) => {
-  const hashtags = value.split(' ');
-  console.log(isHashtagDuplicate(hashtags));
-  if (isHashtagValid(hashtags) && isHashtagDuplicate(hashtags)) {
+  const hashtags = value.toLowerCase().trim().split(' ');
+  if (!hashtags[0] || (isHashtagValid(hashtags) && !isHashtagDuplicate(hashtags) && isAmountLessThan5(hashtags))) {
     uploadSubmitBtn.removeAttribute('disabled');
     return true;
   }
@@ -70,32 +77,8 @@ const validateHashtags = (value) => {
 };
 
 pristine.addValidator(hashtagInput, validateHashtags, () => pristineErrorMsg);
-
-// const isHashtagValid = (hashtag) => regexp.test(hashtag);
-// const isHashtagDuplicate = (hashtags) =>
-// const validateHashtags = (callback, hashtags) => {
-//   hashtags.split(' ').forEach((hashtag) => {
-//     if (callback(hashtag)) {
-//       uploadSubmitBtn.removeAttribute('disabled');
-//       return true;
-//     }
-//     console.log(callback(hashtag));
-//     uploadSubmitBtn.setAttribute('disabled', '');
-//     return false;
-//   });
-// };
-
-// pristine.addValidator(hashtagInput, (value) => {
-//   validateHashtags(regexp.test, value);
-// }, 'Введён невалидный хэштег');
+pristine.addValidator(descriptionField, (value) => value.length !== MAX_SYMBOLS_AMOUNT, `Максимальное количество символов - ${MAX_SYMBOLS_AMOUNT}`);
 
 imgUploadForm.addEventListener('submit', (evt) => {
   evt.preventDefault();
-
-  const isValid = pristine.validate();
-  if (isValid) {
-    //console.log('Можно отправлять');
-  } else {
-    //console.log('Форма невалидна');
-  }
 });
